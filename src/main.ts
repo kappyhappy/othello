027c7e525c195292718089092981f46be5bb09c6
kappyhappy <kaaaappi@gmail.com>
2025-01-16 23:04:22 +0900

    add frontend part

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import express from 'express'
+import morgan from 'morgan'
+import 'express-async-errors'
+import mysql from 'mysql2/promise'
+
+const PORT = 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.static('static', { extensions: ['html'] }))
+
+app.get('/api/hello', async (req, res) => {
+    res.json({
+        message: 'Hello Express!!!'
+    })
+})
+
+app.get('/api/error', async(req, res) => {
+    throw new Error('Error endpoint')
+})
+
+app.post('/api/games', async (req, res) => {
+    res.status(201).end()
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+    console.log(`Othello application started: http://localhost:${PORT}`)
+})
+
+function errorHandler(err: any, _req: express.Request, res: express.Response, _next: express.NextFunction) {
+    console.error('Unexpected error occurred', err)
+    res.status(500).send({
+        message: 'Unexpected error occurred'
+    })
+}
