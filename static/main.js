7c5df69a96133518c0e95b1a3ed8756e135a7f8d
kappyhappy <kaaaappi@gmail.com>
2025-01-23 16:13:23 +0900

    display the winner in frontend

--- static/main.js
+++ static/main.js
@@ -2,25 +2,22 @@ const EMPTY = 0
 const DARK = 1
 const LIGHT = 2
 
-// const INITIAL_BOARD = [
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-//     [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-// ]
+const WINNER_DRAW = 0
+const WINNER_DARK = 1
+const WINNER_LIGHT = 2
 
 const boardElement = document.getElementById('board')
 const nextDiscMessageElement = document.getElementById('next-disc-message')
+const warningMessageElement = document.getElementById('warning-message')
 
-async function showBoard(turnCount){
+async function showBoard(turnCount, previousDisc){
     const response = await fetch(`/api/games/latest/turns/${turnCount}`)
     const responseBody = await response.json()
     const board = responseBody.board
     const nextDisc = responseBody.nextDisc
+    const winnerDisc = responseBody.winnerDisc
+
+    showWarningMessage(previousDisc, nextDisc, winnerDisc)
 
     showNextDiscMessage(nextDisc)
 
@@ -44,7 +41,7 @@ async function showBoard(turnCount){
                     const nextTurnCount = turnCount + 1
                     const registerTurnResponse = await registerTurn(nextTurnCount, nextDisc, x, y)
                     if (registerTurnResponse.ok) {
-                        await showBoard(nextTurnCount)
+                        await showBoard(nextTurnCount, nextDisc)
                     }
                 })
             }
@@ -53,6 +50,49 @@ async function showBoard(turnCount){
         })
     })
 }
+
+function showNextDiscMessage(nextDisc){
+    if (nextDisc) {
+        const color = nextDisc === DARK ? 'Black' : 'White'
+        nextDiscMessageElement.innerText = `Next Turn is ${color}`
+    } else {
+        nextDiscMessageElement.innerText = ''
+    }
+}
+
+function discToString(disc) {
+    return disc === DARK ? 'Black': 'White'
+}
+
+function showWarningMessage(previousDisc, nextDisc, winnerDisc){
+    const message = warningMessage(previousDisc, nextDisc, winnerDisc)
+
+    warningMessageElement.innerText = message
+
+    if (message === null) {
+        warningMessageElement.style.display = 'none'
+    } else {
+        warningMessageElement.style.display = 'block'
+    }
+}
+
+function warningMessage(previousDisc, nextDisc, winnerDisc) {
+    if (nextDisc !== null) {
+        if (previousDisc === nextDisc) {
+            const skipped = nextDisc === DARK ? LIGHT : DARK
+            return `Turn for ${discToString(skipped)} is skipped`
+        } else {
+            return null
+        }
+    } else {
+        if (winnerDisc === WINNER_DRAW) {
+            return 'The result is DRAW !'
+        } else {
+            return `The winner is ${discToString(winnerDisc)} !!`
+        }
+    }
+}
+
 
 async function registerGame() {
     await fetch('/api/games', {
@@ -77,15 +117,6 @@ async function registerTurn(turnCount, disc, x, y){
         },
         body: JSON.stringify(requestBody)
     })
-}
-
-function showNextDiscMessage(nextDisc){
-    if (nextDisc) {
-        const color = nextDisc === DARK ? 'Black' : 'White'
-        nextDiscMessageElement.innerText = `Next Turn is ${color}`
-    } else {
-        nextDiscMessageElement.innerText = ''
-    }
 }
 
 async function main(){
