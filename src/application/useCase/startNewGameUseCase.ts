120ff9a873f10db9c115b80578e1f9e2279dd08d
kappyhappy <kaaaappi@gmail.com>
2025-01-24 10:26:15 +0900

    add DI and modify service to usecase

--- src/application/useCase/startNewGameUseCase.ts
+++ src/application/useCase/startNewGameUseCase.ts
@@ -3,19 +3,26 @@ import { connectMySQL } from "../../infrastructure/connection"
 import { firstTurn } from "../../domain/model/turn/turn"
 import { GameMySQLRepository } from "../../infrastructure/repository/game/gameMySQLRepository"
 import { TurnMySQLRepository } from "../../infrastructure/repository/turn/turnMySQLRepository"
+import { GameRepository } from "../../domain/model/game/gameRepository"
+import { TurnRepository } from "../../domain/model/turn/turnRepository"
 
-const gameRepository = new GameMySQLRepository()
-const turnRepository = new TurnMySQLRepository()
+// const gameRepository = new GameMySQLRepository()
+// const turnRepository = new TurnMySQLRepository()
 
-export class GameService {
-    async startNewGame() {
+export class StartNewGameUseCase {
+    constructor(
+        private _gameRepository: GameRepository,
+        private _turnRepository: TurnRepository
+    ) {}
+
+    async run() {
         const now = new Date()
 
         const conn = await connectMySQL()
         try {
             await conn.beginTransaction()
             
-            const game = await gameRepository.save(
+            const game = await this._gameRepository.save(
                 conn, new Game(undefined, now)
             )
             if (!game.id) {
@@ -24,7 +31,7 @@ export class GameService {
 
             const turn = firstTurn(game.id, now)
 
-            await turnRepository.save(conn, turn)
+            await this._turnRepository.save(conn, turn)
     
             await conn.commit()
         } finally {
--- src/presentation/turnRouter.ts
+++ src/presentation/turnRouter.ts
@@ -1,11 +1,24 @@
 import express from "express"
-import { TurnService } from "../application/service/turnService"
 import { Point } from "../domain/model/turn/point"
 import { toDisc } from "../domain/model/turn/disc"
+import { TurnMySQLRepository } from "../infrastructure/repository/turn/turnMySQLRepository"
+import { GameMySQLRepository } from "../infrastructure/repository/game/gameMySQLRepository"
+import { GameResultMySQLRepository } from "../infrastructure/repository/gameResult/gameResultMySQLRepository"
+import { RegisterTurnUseCase } from "../application/useCase/registerTurnUseCase"
+import { FindLatestGameTurnByTurnCountUseCase } from "../application/useCase/findLatestGameTurnByTurnCountUseCase"
 
 export const turnRouter = express.Router()
 
-const turnService = new TurnService()
+const findLatestGameTurnByTurnUseCase = new FindLatestGameTurnByTurnCountUseCase(
+    new TurnMySQLRepository(),
+    new GameMySQLRepository(),
+    new GameResultMySQLRepository()
+)
+const registerTurnUseCase = new RegisterTurnUseCase(
+    new TurnMySQLRepository(),
+    new GameMySQLRepository(),
+    new GameResultMySQLRepository()
+)
 
 interface TurnGetResponseBody {
     turnCount: number
@@ -18,7 +31,7 @@ turnRouter.get(
     '/api/games/latest/turns/:turnCount', 
     async (req, res: express.Response<TurnGetResponseBody>) => {
         const turnCount = parseInt(req.params.turnCount)
-        const output = await turnService.findLatestGameTurnByTurnCount(turnCount)
+        const output = await findLatestGameTurnByTurnUseCase.run(turnCount)
 
         const responseBody = {
             turnCount: output.turnCount,
@@ -47,7 +60,7 @@ turnRouter.post(
         const disc = toDisc(req.body.move.disc)
         const point = new Point(req.body.move.x, req.body.move.y)
         
-        await turnService.registerTurn(
+        await registerTurnUseCase.run(
             turnCount, disc, point
         )
 
